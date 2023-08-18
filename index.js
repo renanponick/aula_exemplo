fe0ca41b9c4eba9e6851d5fe89abb5e759794f07
renanponick <--list>
2023-08-17 21:44:36 -0300

    feat: add 3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { somar } from './exercicios/um.js'
 import { sub } from './exercicios/dois.js'
+import { div } from './exercicios/tres.js'
 
 const app = express()
 app.use(express.json())
@@ -19,6 +20,14 @@ app.post('/api/exercicio2', (req, res) => {
         message: `resultado: ${result}`
     })
 })
+
+app.post('/api/exercicio3', (req, res) => {
+    const result = div(req.body.num1, req.body.num2)
+    res.status(200).json({
+        message: `resultado: ${result}`
+    })
+})
+
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
