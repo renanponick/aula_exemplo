eec8b62097e0b1a210e31d9fdfec6794db16d3c0
renanponick <--list>
2023-08-17 21:42:09 -0300

    feat: add exercicio 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { somar } from './exercicios/um.js'
+import { sub } from './exercicios/dois.js'
 
 const app = express()
 app.use(express.json())
@@ -7,6 +8,13 @@ const port = 3000
 
 app.post('/api/exercicio', (req, res) => {
     const result = somar(req.body.num1, req.body.num2)
+    res.status(200).json({
+        message: `resultado: ${result}`
+    })
+})
+
+app.post('/api/exercicio2', (req, res) => {
+    const result = sub(req.body.num1, req.body.num2)
     res.status(200).json({
         message: `resultado: ${result}`
     })
