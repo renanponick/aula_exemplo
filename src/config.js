69e31597ea899ed74defe60dc7f3d4147204ff9e
renanponick <--list>
2023-09-29 19:58:53 -0300

    add dot env

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 module.exports = {
     development: {
-        username: 'root',
-        password: '',
-        database: 'exemplo',
-        host: 'localhost',
-        dialect: 'mysql'
+        username: process.env.USER,
+        password:  process.env.PASSWORD,
+        database: process.env.DATABASE_URL,
+        host: process.env.HOST,
+        dialect: process.env.DIALECT
     }
 }
