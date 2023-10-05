0c9c151bfe08d7e66cd64a27382a96b92a13633e
renan <warreir@gmail.com>
2023-10-04 21:27:11 -0300

    fix teste

--- test/index.test.js
+++ test/index.test.js
@@ -4,8 +4,6 @@ const conexao = require('../src/database')
 
 describe('Testes pessoas', () => {
 
-   let save
-   
    beforeAll(async () => {
       this.save = await conexao.transaction()
    })
@@ -18,9 +16,9 @@ describe('Testes pessoas', () => {
    it('Should get person', async () => {
       const result = await servico.PegarUm(1, this.save)
 
-      expect(result.id).toBe(this.id);
+      expect(result.id).toBe(1);
       expect(result.nome).toBe('joao');
-      expect(result.email).toBe('teste2@teste.com');
+      expect(result.email).toBe('teste@teste.com');
       expect(result.senha).toBe('123456');
    })
 
@@ -30,8 +28,6 @@ describe('Testes pessoas', () => {
          email: 'teste2@teste.com',
          senha: '123456'
       }, this.save)
-
-      this.id = result.id
 
       expect(result.nome).toBe('joao');
       expect(result.email).toBe('teste2@teste.com');
